e6ab8910e1a65208595e11679306905646589932
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2024-08-26 21:34:02 -0400

    Hash-map: Learned the map.has() method

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ let map = new Map();
 
 map.set("name", "John");
 map.set("age", 30);
+map.set("preferredFruit", "Avocato")
+map.set("School", "U of T")
+map.set("City", "London")
+map.set("Course Taken", 10)
 
+// This is the map.get method
 console.log(map.get('name'))
 console.log(map.get('age'))
-console.log("My name is ", map.get("name"), " " + "and I am ", map.get('age'))
+console.log("My name is ", map.get("name"), " " + "and I am ", map.get('age'));
+
+// We can also use the map.has(): This will check if a specific value is included
+
+console.log(map.has("Course Taken"), " It has");
+console.log(map.has("City"), " It does have")
+console.log(map.has("Education "), " it doesn't have it")
